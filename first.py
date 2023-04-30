c9bd3143faaeb841d5ade1e824afbf8a6da6f3d6
Deepthi <dagududeepthi@gmail.com>
2023-04-30 14:43:42 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,10 @@
+book1 = 499
+book2 = 855
+book3 = 645
+deliveryCharges = 250
+b1 = int(input("Enter number of books :"))
+b2 = int(input("Enter number of books :"))
+b3 = int(input("Enter number of books :"))
+total = (b1*book1)+(b2*book2)+(b3*book3)
+totalAmt = total + (total * 0.12) + deliveryCharges
+print("Total invoice Amount : ",totalAmt)
